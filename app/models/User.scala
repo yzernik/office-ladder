92599b0ccb61e45426e8519351678d3bdde71d97
yzernik <yzernik@gmail.com>
2015-03-23 08:17:25 -0700

    added slick user model mapping

--- app/models/User.scala
+++ app/models/User.scala
@@ -1,11 +1,18 @@
 package models
 
 import java.util.UUID
-
 import com.mohiva.play.silhouette.core.Identity
 import com.mohiva.play.silhouette.core.LoginInfo
-
 import play.api.libs.json.Json
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Await
+import scala.concurrent.duration.Duration
+
+import slick.driver.H2Driver.api._
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Await
+import scala.concurrent.duration.Duration
 
 /**
  * The user object.
@@ -16,7 +23,6 @@ import play.api.libs.json.Json
  * @param email The email of the authenticated provider.
  */
 case class User(
-  userID: UUID,
   loginInfo: LoginInfo,
   firstName: String,
   lastName: String,
@@ -35,10 +41,36 @@ object User {
   import play.api.libs.json._
   import play.api.libs.functional.syntax._
 
+  /*
+  def apply(providerName: String,
+            providerKey: String,
+            email: String,
+            firstName: String,
+            lastName: String,
+            fullName: String,
+            avatarUrl: Option[String]): User = {
+    val loginInfo = LoginInfo(providerName, providerKey)
+    User(loginInfo, email, firstName, lastName, fullName, avatarUrl)
+  }
+*/
+
   // create the formats object for LoginInfo.
   implicit val loginInfoFormats = Json.format[LoginInfo]
 
   // create the formats object for LoginInfo.
   implicit val userFormats = Json.format[User]
+}
 
+// Definition of the Users table
+class Suppliers(tag: Tag) extends Table[User](tag, "users") {
+  def email = column[String]("email", O.PrimaryKey) // This is the primary key column
+  def firstName = column[String]("firstname")
+  def lastName = column[String]("lastname")
+  def fullName = column[String]("fullname")
+  def avatarUrl = column[String]("avatarurl")
+  def providerName = column[String]("providername")
+  def providerKey = column[String]("providerkey")
+
+  // Every table needs a * projection with the same type as the table's type parameter
+  def * = (providerName, providerKey, email, firstName, lastName, fullName, avatarUrl.?) <> ((User.apply _).tupled, User.unapply)
 }
