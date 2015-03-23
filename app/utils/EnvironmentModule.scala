5bd74fe7f20328942801e02c42b324d81d31bd65
yzernik <yzernik@gmail.com>
2015-03-22 18:26:44 -0700

    added social authentication provider

--- app/utils/EnvironmentModule.scala
+++ app/utils/EnvironmentModule.scala
@@ -6,27 +6,28 @@ import com.mohiva.play.silhouette.contrib.utils.PlayCacheLayer
 import com.mohiva.play.silhouette.contrib.utils.SecureRandomIDGenerator
 import com.mohiva.play.silhouette.core.Environment
 import com.mohiva.play.silhouette.core.EventBus
+import com.mohiva.play.silhouette.core.utils.PlayHTTPLayer
 
 import models.User
-import models.daos.PasswordInfoDAO
 
 trait EnvironmentModule
   extends AuthenticatorServiceModule
   with UserServiceModule
   with AuthInfoServiceModule
-  with CredentialsProviderModule {
+  with SocialProviderModule {
 
   lazy val cacheLayer = new PlayCacheLayer
+  lazy val httpLayer = new PlayHTTPLayer
   lazy val eventBus = EventBus()
   lazy val idGenerator = new SecureRandomIDGenerator
-  lazy val passwordInfoDAO = new PasswordInfoDAO
+  lazy val authInfoDAO = ???
   lazy val passwordHasher = new BCryptPasswordHasher
   implicit lazy val env: Environment[User, CachedCookieAuthenticator] = {
     Environment[User, CachedCookieAuthenticator](
       userService,
       authenticatorService,
       Map(
-        credentialsProvider.id -> credentialsProvider),
+        "google" -> socialProvider),
       eventBus)
   }
 
