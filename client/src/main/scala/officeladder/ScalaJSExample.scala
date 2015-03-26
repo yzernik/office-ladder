dfdb59a9c81304aadb3785b6805adcf5da1513f2
yzernik <yzernik@gmail.com>
2015-03-26 02:30:13 -0700

    fix dependency on bootstrap

--- client/src/main/scala/officeladder/ScalaJSExample.scala
+++ client/src/main/scala/officeladder/ScalaJSExample.scala
@@ -1,10 +1,17 @@
 package officeladder
 
 import scala.scalajs.js
+
 import org.scalajs.dom
+import japgolly.scalajs.react.vdom.prefix_<^._
 
 object ScalaJSExample extends js.JSApp {
   def main(): Unit = {
     dom.document.getElementById("scalajsShoutOut").textContent = "Hello world!"
   }
+
+  val vdom = <.a(
+    ^.className := "google",
+    ^.href := "https://www.google.com",
+    <.span("GOOGLE!"))
 }
