b6d228b326e4851cfc849fe6cf90b575ec7d7332
yzernik <yzernik@gmail.com>
2015-03-26 23:38:41 -0700

    use non-trivial react example

---
+++ client/src/main/scala/officeladder/CreateLadder.scala
@@ -0,0 +1,5 @@
+package officeladder
+
+class CreateLadder {
+
+}
--- client/src/main/scala/officeladder/ScalaJSExample.scala
+++ client/src/main/scala/officeladder/ScalaJSExample.scala
@@ -9,17 +9,6 @@ import japgolly.scalajs.react.vdom.prefix_<^._
 
 object ScalaJSExample extends js.JSApp {
   def main(): Unit = {
-    dom.document.getElementById("scalajsShoutOut").textContent = "Hello world!"
-    HelloMessage("testing...").render(dom.document.getElementById("scalajsShoutOut"))
+    ProductTableExample.content.render(dom.document.getElementById("scalajsShoutOut"))
   }
-
-  val vdom = <.a(
-    ^.className := "google",
-    ^.href := "https://www.google.com",
-    <.span("GOOGLE!"))
-
-  val HelloMessage = ReactComponentB[String]("HelloMessage")
-    .render(name => <.div("Hello ", name))
-    .build
-
 }
