368a4af53f16edefd216a29c025b274f116bb3c4
yzernik <yzernik@gmail.com>
2015-04-22 21:58:55 -0700

    organized js code export

--- client/src/main/scala/officeladder/OfficeLadderClient.scala
+++ client/src/main/scala/officeladder/OfficeLadderClient.scala
@@ -1,13 +1,14 @@
 package officeladder
 
 import scala.scalajs.js
+import scala.scalajs.js.annotation.JSExport
 
 import org.scalajs.dom
 
 import japgolly.scalajs.react._
 import japgolly.scalajs.react.vdom.all._
 
-object ScalaJSExample extends js.JSApp {
+object OfficeLadderClient extends js.JSApp {
 
   val container = ReactComponentB[Unit]("homePage")
     .render(P => {
@@ -16,6 +17,15 @@ object ScalaJSExample extends js.JSApp {
     }).buildU
 
   def main(): Unit = {
-    container().render(dom.document.getElementById("scalajsShoutOut"))
+    // do nothing!
   }
+
+  def renderClient[P, S, B, N <: TopNode](c: ReactComponentU[P, S, B, N]) = {
+    c.render(dom.document.getElementById("client"))
+  }
+
+  @JSExport
+  def renderHome(): Unit =
+    renderClient(container())
+
 }
